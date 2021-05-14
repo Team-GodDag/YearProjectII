9fa4c733e06137f0bb07de89a5275cfea79762e4
Champen <dawgen@gmail.com>
2021-05-14 10:12:56 +0200

    Main cleaned for imports

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,12 +2,7 @@ package view;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import logic.CreditRateToInterestRate;
-import logic.WriteCSV;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Main extends Application {
 
@@ -22,11 +17,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        //get current date time with Date()
-        Date date = new Date();
-        System.out.println(dateFormat.format(date));
         launch(args);
 
     }
