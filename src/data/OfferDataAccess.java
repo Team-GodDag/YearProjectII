95e773c61f7cb394287ebe9cfc04967de7e23bb3
rikkerorbak <rikke.rorbak@gmail.com>
2021-06-07 15:45:57 +0200

    removed out-commented stuff

--- src/data/OfferDataAccess.java
+++ src/data/OfferDataAccess.java
@@ -6,16 +6,12 @@ import entities.Offer;
 import java.util.ArrayList;
 
 public interface OfferDataAccess {
-    //Laver af Rikke
+    //Lavet af Rikke
     ArrayList<Offer> getAllOffers();
 
     boolean addOffer(Offer offer);
 
     boolean deleteOffer(Offer offer);
-
-//    boolean updateOffer(Offer offer);
-
-//    ArrayList<Offer> getOffersByCondition(String condition);
 
     ArrayList<Offer> getOffersByCustomer(Customer customer);
 }
